8e04c758c14568b5d9c17c58f64252784477be33
Achintya <achintyatiwari27>
2021-09-14 02:56:22 +0530

    [Refactored]:Code to have unique cards

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,7 +1,24 @@
 package com.BridgeLabz;
 
+import java.util.Scanner;
+
 public class DeckOfCards {
     public static void main(String[] args) {
-        System.out.println("Welcome to Deck of Cards game program !!");
+        System.out.println("Welcome to Deck of Cards gaming program :) ");
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[] player = {"Player 1", "Player 2", "Player 3", "Player 4"};
+        String[][] deck = new String[suits.length][ranks.length];
+        int card = 0;
+
+        for (int i = 0; i < suits.length; i++) {
+            for (int j = 0; j < ranks.length; j++) {
+                deck[i][j] = suits[i] + " " + ranks[j];
+                System.out.println(deck[i][j]);
+                card++;
+            }
+        }
+        System.out.println("Total number of cards are:" + card);
     }
 }
+
