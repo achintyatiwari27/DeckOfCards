9a5b48dfd067859af59426270ddca1c36d198a14
Achintya <achintyatiwari27>
2021-09-14 03:00:37 +0530

    [Refactored]:Code to add players

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -19,6 +19,17 @@ public class DeckOfCards {
             }
         }
         System.out.println("Total number of cards are:" + card);
+        addPlayers();
+    }
+
+    public static void addPlayers() {
+        System.out.println("Enter number of players want to play the game:");
+        Scanner sc =new Scanner(System.in);
+        int player = sc.nextInt();
+        if (player >= 2 && player <= 4) {
+            System.out.println("No.of Players to play the game : " + player);
+        } else {
+            System.out.println("Error !!! \n Minimum 2 and Maximum 4 players are allowed");
+        }
     }
 }
-
